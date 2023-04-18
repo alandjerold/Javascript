fd7c1fdef59bd5f0a67cd35b25c9d654c3213a99
AlanVieira <alanvieira30@outlook.com>
2023-04-18 12:17:53 -0300

    att

--- exercícios/desafio05/script2.js
+++ exercícios/desafio05/script2.js
@@ -1,27 +1,75 @@
-function adicionar(){
+
     var n1 = document.getElementById('n1')
     var res = document.getElementById('res')
     var res2 = document.getElementById('res2')
-    var c = 1
-    var n = Number(n1.value)
-    var r = Number(res.value)
-
-    if(n > 100 && n !== r ){
-        window.alert('[Erro] número já inserido ou número inválido')
+    var valores = []
+    
+    
+    function isnumber(n){
+        if(n >= 1 && n <= 100){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    function list(n, v){
+        if(v.indexOf(Number(n)) != -1){
+        return true
+        
+    }
+    else{
+        return false
+    }
     }
     
-     else if(n <= 100){
+
+function adicionar(){
+    if(isnumber(n1.value) && !list(n1.value, valores) ){
+        valores.push(Number(n1.value))
         var item = document.createElement('option')
         res.appendChild(item)
-        item.innerHTML=(`Valor ${n} adicionado`)
-            
-     }
-     
-     function somar(){
-        if(res>=1){
-            
-            res2.innerHTML(`a${res}`)
+        
+        item.text = (`Valor ${n1.value} adicionado`)
+        res2.innerHTML = ''
+        
+    }
+    else  {
+        window.alert('Erro')
+        
+    
+}
+    n1.value = ''
+    n1.focus()
+    //usado para não ter que clicar na caixa do numero para ter que escrever
+    //após add um numero ele apaga o numero que esta escrito na barra de numero
+}
 
+function somar(){
+    if(valores.length == 0 ){
+        window.alert('Erro')
+    }
+    else{
+        var tota = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        
+        for(var aps in valores){
+            if(valores[aps] > maior)
+            maior = valores[aps]
+            if(valores[aps] < menor)
+            menor = valores[aps]
+            soma += valores[aps]
+
+            media = soma / tota
+            
         }
+        res2.innerHTML =(`<p>Estão cadastrados ${tota} valores</p>`)
+        res2.innerHTML +=(`<p>O valor maior é o ${maior}</p>`)
+        res2.innerHTML += (`<p>O valor menor é o ${menor}</p>`)
+        res2.innerHTML += (`<p>A soma dos valores é de ${soma}</p>`)
+        res2.innerHTML += (`<p>A média é de ${media}</p>`)
     }
 }
